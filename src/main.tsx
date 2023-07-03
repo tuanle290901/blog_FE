b9bf635197f17ce4be31b7c37c9cdb34a1d16814
TuNguyen <vantu1998@gmail.com>
2023-07-03 15:48:53 +0700

    chore: init project

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
 import { Provider } from 'react-redux'
-import { store } from '~/app/store.ts'
+import { store } from '~/stores/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
