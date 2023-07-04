0cb2cab2d27784bdf195e60d1670c9ec5a466952
hungpham1998 <hungrost@gmail.com>
2023-07-04 09:25:58 +0700

    chore: add package i18n translate language

---
+++ src/i18n.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-named-as-default-member */
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import commonVN from './translations/vn/common.json'
+import commonEN from './translations/en/common.json'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    },
+    resources: {
+      en: { translation: { ...commonEN } },
+      vi: { translation: { ...commonVN } }
+    }
+  })
+
+export default i18n
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from './App.tsx'
 import './index.scss'
 import { Provider } from 'react-redux'
 import { store } from '~/stores/store.ts'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
     </Provider>
   </React.StrictMode>
 )
