90deba28a3a494c54c712f0bab976a5a815970c1
TuNguyen <vantu1998@gmail.com>
2023-07-04 08:53:33 +0700

    feat: add i18n and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import './App.scss'
-import { useTranslation } from 'react-i18next'
+import { MainRoutes } from '~/routes/mainRoutes.tsx'
 
 const App: React.FC = () => {
-  const [t] = useTranslation()
-  return (
-    <h1 className='tw-text-3xl tw-text-center tw-text-red-500 tw-mt-10 tw-w-full tw-font-bold tw-underline'>
-      {t('common.title')}
-    </h1>
-  )
+  return <MainRoutes />
 }
 
 export default App
