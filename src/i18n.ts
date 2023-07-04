5ce3031cc8876059c41c8720c120641896a2665a
hungpham1998 <hungrost@gmail.com>
2023-07-04 09:29:51 +0700

    chore: add package i18n translate language

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-named-as-default-member */
 import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import commonVN from './translations/vn/common.json'
+import { initReactI18next } from 'react-i18next'
 import commonEN from './translations/en/common.json'
+import commonVN from './translations/vn/common.json'
 
 i18n
   .use(LanguageDetector)
