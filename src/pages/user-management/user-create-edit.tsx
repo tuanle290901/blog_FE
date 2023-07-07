2d1597baa14afe42e61b23feefe09a43f7a27012
TuNguyen <vantu1998@gmail.com>
2023-07-07 11:27:16 +0700

    feat(create-user): add dynamic rule for create user form

--- src/pages/user-management/user-create-edit.tsx
+++ src/pages/user-management/user-create-edit.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Button, DatePicker, Form, Input, message, Modal, Select, Upload, UploadFile, UploadProps } from 'antd'
-import { EditOutlined } from '@ant-design/icons'
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
 import { useTranslation } from 'react-i18next'
 import { RcFile, UploadChangeParam } from 'antd/es/upload'
 import defaultImg from '~/assets/images/default-img.png'
@@ -47,7 +47,8 @@ const UserCreateEdit: React.FC<{ open: boolean; handleClose: () => void; userDat
   }, [userData])
   const handleSubmit = () => {
     const value = form.getFieldsValue()
-    handleClose()
+    // handleClose()
+    console.log(value)
   }
   const handleClickButtonUpdateAvatar = () => {
     if (uploadRef?.current) {
@@ -64,10 +65,10 @@ const UserCreateEdit: React.FC<{ open: boolean; handleClose: () => void; userDat
       cancelText={t('common.cancel')}
       maskClosable={false}
       forceRender
-      width={900}
+      width={1000}
       centered
     >
-      <div className='tw-max-h-[calc(100vh-260px)] tw-overflow-auto'>
+      <div className='tw-max-h-[calc(100vh-214px)] tw-min-w-[800px] tw-overflow-auto'>
         <div className='tw-flex tw-items-center tw-gap-4'>
           <Upload
             name='avatar'
@@ -110,19 +111,19 @@ const UserCreateEdit: React.FC<{ open: boolean; handleClose: () => void; userDat
             <div className='tw-w-1/2'>
               <h3 className='tw-py-3 tw-font-semibold tw-text-sm'>{t('userList.commonInfo')}</h3>
               <div className='tw-p-4 tw-bg-[#FAFAFA]'>
-                <Form.Item style={{ marginBottom: 8 }} label={t('userList.username')} name='username' required>
+                <Form.Item style={{ marginBottom: 16 }} label={t('userList.username')} name='username' required>
                   <Input placeholder={t('userModal.enterUserName')} />
                 </Form.Item>
-                <Form.Item style={{ marginBottom: 8 }} label={t('userList.fullName')} name='fullName' required>
+                <Form.Item style={{ marginBottom: 16 }} label={t('userList.fullName')} name='fullName' required>
                   <Input placeholder={t('userModal.enterMemberName')} />
                 </Form.Item>
-                <Form.Item style={{ marginBottom: 8 }} label={t('userList.gender')} name='gender'>
+                <Form.Item style={{ marginBottom: 16 }} label={t('userList.gender')} name='gender'>
                   <Select placeholder={t('userModal.selectGender')}>
                     <Select.Option value='male'>{t('userList.male')}</Select.Option>
                     <Select.Option value='female'>{t('userList.female')}</Select.Option>
                   </Select>
                 </Form.Item>
-                <Form.Item style={{ marginBottom: 8 }} label={t('userList.dateOfBirth')} name='dateOfBirth'>
+                <Form.Item style={{ marginBottom: 16 }} label={t('userList.dateOfBirth')} name='dateOfBirth'>
                   <DatePicker
                     format='YYYY/MM/DD'
                     disabledDate={(date) => {
@@ -133,10 +134,10 @@ const UserCreateEdit: React.FC<{ open: boolean; handleClose: () => void; userDat
                     placeholder={t('userModal.selectDOB')}
                   />
                 </Form.Item>
-                <Form.Item style={{ marginBottom: 8 }} label={t('userList.phoneNumber')} name='phoneNumber'>
+                <Form.Item style={{ marginBottom: 16 }} label={t('userList.phoneNumber')} name='phoneNumber'>
                   <Input placeholder={t('userModal.enterPhoneNumber')} />
                 </Form.Item>
-                <Form.Item style={{ marginBottom: 8 }} label={t('userList.email')} name='email'>
+                <Form.Item style={{ marginBottom: 16 }} label={t('userList.email')} name='email'>
                   <Input placeholder={t('userModal.enterEmail')} />
                 </Form.Item>
                 <Form.Item style={{ marginBottom: 0 }} label={t('userList.address')} name='address'>
@@ -147,7 +148,7 @@ const UserCreateEdit: React.FC<{ open: boolean; handleClose: () => void; userDat
             <div className='tw-w-1/2'>
               <h3 className='tw-py-3 tw-font-semibold tw-text-sm'>{t('userList.workInfo')}</h3>
               <div className='tw-p-4 tw-bg-[#FAFAFA] tw-h-[482px] tw-overflow-auto'>
-                <Form.Item style={{ marginBottom: 8 }} label={t('userList.dateJoin')} name='dateJoin'>
+                <Form.Item style={{ marginBottom: 16 }} label={t('userList.dateJoin')} name='dateJoin'>
                   <DatePicker
                     format='YYYY/MM/DD'
                     disabledDate={(date) => {
@@ -158,7 +159,7 @@ const UserCreateEdit: React.FC<{ open: boolean; handleClose: () => void; userDat
                     placeholder={t('userModal.probationDate')}
                   />
                 </Form.Item>
-                <Form.Item style={{ marginBottom: 8 }} label={t('userList.probationDate')} name='probationDate'>
+                <Form.Item style={{ marginBottom: 16 }} label={t('userList.probationDate')} name='probationDate'>
                   <DatePicker
                     format='YYYY/MM/DD'
                     disabledDate={(date) => {
@@ -170,7 +171,7 @@ const UserCreateEdit: React.FC<{ open: boolean; handleClose: () => void; userDat
                   />
                 </Form.Item>
                 <Form.Item
-                  style={{ marginBottom: 8 }}
+                  style={{ marginBottom: 16 }}
                   label={t('userList.officialContractSigningDate')}
                   name='officialContractSigningDate'
                 >
@@ -184,6 +185,52 @@ const UserCreateEdit: React.FC<{ open: boolean; handleClose: () => void; userDat
                     placeholder={t('userModal.enterOfficialContractSigningDate')}
                   />
                 </Form.Item>
+                <Form.List name='users'>
+                  {(fields = [], { add, remove }) => (
+                    <>
+                      {fields.map(({ key, name, ...restField }, index) => (
+                        <div key={key}>
+                          <Form.Item
+                            {...restField}
+                            style={{ marginBottom: 16 }}
+                            label={t('userList.department') + ' ' + index}
+                            name={[name, 'department']}
+                            required
+                          >
+                            <MinusCircleOutlined
+                              className='tw-absolute -tw-top-[24px] -tw-right-0 tw-z-50 tw-text-red-600'
+                              onClick={() => remove(name)}
+                            />
+                            <Select placeholder={t('userModal.selectDepartment')}></Select>
+                          </Form.Item>
+                          <div className='tw-grid tw-grid-cols-2 tw-gap-2'>
+                            <Form.Item
+                              {...restField}
+                              style={{ marginBottom: 16 }}
+                              name={[name, 'first']}
+                              rules={[{ required: true, message: 'Missing first name' }]}
+                            >
+                              <Select placeholder={t('userModal.selectPosition')}></Select>
+                            </Form.Item>
+                            <Form.Item
+                              {...restField}
+                              style={{ marginBottom: 16 }}
+                              name={[name, 'last']}
+                              rules={[{ required: true, message: 'Missing last name' }]}
+                            >
+                              <Select placeholder={t('userModal.selectFunction')}></Select>
+                            </Form.Item>
+                          </div>
+                        </div>
+                      ))}
+                      <Form.Item>
+                        <Button type='dashed' onClick={() => add()} block icon={<PlusOutlined />}>
+                          {t('userModal.addRole')}
+                        </Button>
+                      </Form.Item>
+                    </>
+                  )}
+                </Form.List>
               </div>
             </div>
           </div>
