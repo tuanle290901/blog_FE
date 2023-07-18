083ee79742afdf238ad8af5d792e38ad083c8842
hungpham1998 <hungrost@gmail.com>
2023-07-18 15:54:46 +0700

    chore: fix bug department and device

--- src/pages/Department/DepartmentModal.tsx
+++ src/pages/Department/DepartmentModal.tsx
@@ -1,9 +1,11 @@
-import { DatePicker, Form, Input, Modal } from 'antd'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { DatePicker, Form, Input, Modal, notification } from 'antd'
+import { AxiosResponse, HttpStatusCode } from 'axios'
 import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { REGEX_EMAIL, REGEX_PHONE_NUMBER, REGEX_TRIM } from '~/constants/regex.constant'
 import { createDepartment, getListDepartments, updateDepartment } from '~/stores/features/department/department.silce'
-import { useAppDispatch } from '~/stores/hook'
+import { useAppDispatch, useAppSelector } from '~/stores/hook'
 import { IDepartment, IDepartmentModal } from '~/types/department.interface'
 import { ACTION_TYPE } from '~/utils/helper'
 
@@ -12,7 +14,6 @@ const DepartmentModal: React.FC<IDepartmentModal> = (props) => {
   const { t } = useTranslation()
   const [form] = Form.useForm()
   const dispatch = useAppDispatch()
-
   const onSaveData = async () => {
     const { code, address, name, publishDate, contactEmail, contactPhoneNumber } = form.getFieldsValue()
     let payload: IDepartment = {
@@ -31,16 +32,40 @@ const DepartmentModal: React.FC<IDepartmentModal> = (props) => {
         code: data.code
       }
       await dispatch(updateDepartment(payload))
+        .unwrap()
+        .then(async (response) => {
+          notification.success({
+            message: response.message
+          })
+          form.resetFields()
+          await onOk()
+        })
+        .catch((error) => {
+          notification.error({
+            message: error.message
+          })
+        })
     } else {
       payload = {
         ...payload,
-        parentCode: dataParent && dataParent[dataParent.length - 1].code
+        parentCode: dataParent && dataParent[dataParent.length > 2 ? dataParent.length - 1 : 0].code
       }
-      await dispatch(createDepartment(payload))
-      dispatch(getListDepartments())
+      dispatch(createDepartment(payload))
+        .unwrap()
+        .then(async (response) => {
+          notification.success({
+            message: response.message
+          })
+          form.resetFields()
+          await onOk()
+          dispatch(getListDepartments())
+        })
+        .catch((error) => {
+          notification.error({
+            message: error.message
+          })
+        })
     }
-    form.resetFields()
-    await onOk()
   }
 
   useEffect(() => {
@@ -69,71 +94,80 @@ const DepartmentModal: React.FC<IDepartmentModal> = (props) => {
       forceRender
       closable
       onCancel={() => onCancel()}
-      onOk={() => onSaveData()}
-      title={typeModel === ACTION_TYPE.Created ? 'Thêm mới hoặc chính sửa departmanet' : 'Cập nhật ...'}
+      onOk={form.submit}
+      okText={`${t('common.save')}`}
+      cancelText={`${t('common.cancel')}`}
+      title={
+        typeModel === ACTION_TYPE.Created
+          ? `${t('department.departmentAdd')}`
+          : `${t('department.departmentUpdate', { name: data && data.name })}`
+      }
       maskClosable={false}
     >
-      <Form form={form} layout='vertical'>
+      <Form form={form} layout='vertical' onFinish={onSaveData}>
         <Form.Item
           name='code'
-          label={t('Mã phòng ban')}
+          label={t('department.code')}
           rules={[
             {
               required: true,
-              message: `${t('campaign.please-insert-input')} ${t('campaign.campaign-name')}`
+              message: `${t('department.please-insert-input')} ${t('department.code')}`
             },
             {
               pattern: REGEX_TRIM,
-              message: `${t('campaign.alter-notification.do-not-leave-spaces-special-characters-vietnamese-accents')}`
+              message: `${t('department.alter-notification.do-not-leave-spaces-special-accents')}`
             }
           ]}
         >
-          <Input disabled={typeModel === ACTION_TYPE.Created ? false : true} />
+          <Input
+            disabled={typeModel === ACTION_TYPE.Created ? false : true}
+            placeholder={`${t('department.please-insert-input')}`}
+          />
         </Form.Item>
         <Form.Item
           name='name'
-          label={t('Tên phòng ban')}
+          label={t('department.name')}
           rules={[
             {
               required: true,
-              message: `${t('campaign.please-insert-input')} ${t('campaign.campaign-name')}`
+              message: `${t('department.please-insert-input')} ${t('department.name')}`
             },
             {
               pattern: REGEX_TRIM,
-              message: `${t('campaign.alter-notification.do-not-leave-spaces-special-characters-vietnamese-accents')}`
+              message: `${t('department.alter-notification.do-not-leave-spaces-special-characters-vietnamese-accents')}`
             }
           ]}
         >
-          <Input />
+          <Input placeholder={`${t('department.please-insert-input')}`} />
         </Form.Item>
         <Form.Item
           name='contactEmail'
-          label={t('Email phòng ban')}
+          label={t('department.email')}
           rules={[
             {
               pattern: REGEX_EMAIL,
-              message: `${t('campaign.alter-notification.do-not-leave-spaces-special-characters-vietnamese-accents')}`
+              message: `${t('department.alter-notification.do-not-email-accents')}`
             }
           ]}
         >
-          <Input />
+          <Input placeholder={`${t('department.please-insert-input')}`} />
         </Form.Item>
         <Form.Item
           name='contactPhoneNumber'
-          label={t('Số điện thoại')}
+          label={t('department.phone-number')}
           rules={[
             {
               pattern: REGEX_PHONE_NUMBER,
-              message: `${t('campaign.alter-notification.do-not-leave-spaces-special-characters-vietnamese-accents')}`
+              message: `${t('department.alter-notification.do-not-phone-accents')}`
             }
           ]}
         >
-          <Input />
+          <Input placeholder={`${t('department.please-insert-input')}`} />
         </Form.Item>
-        <Form.Item name='address' label={t('Địa chỉ')}>
-          <Input />
+        <Form.Item name='address' label={t('department.address')}>
+          <Input placeholder={`${t('department.please-insert-input')}`} />
         </Form.Item>
-        <Form.Item labelAlign='left' name='publishDate' label={t('Ngày thành lập')}>
+        <Form.Item labelAlign='left' name='publishDate' label={t('department.publishDate')}>
           <DatePicker className='tw-w-[100%]' />
         </Form.Item>
       </Form>
