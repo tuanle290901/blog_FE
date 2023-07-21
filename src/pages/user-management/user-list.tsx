074aea4e0aa8e0f6158f1c4c3b02e498ec13a979
TuNguyen <vantu1998@gmail.com>
2023-07-21 16:43:00 +0700

    fix(role): change type of interface

--- src/pages/user-management/user-list.tsx
+++ src/pages/user-management/user-list.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { Button, Input, notification, Select, Table, TablePaginationConfig } from 'antd'
-import { DeleteOutlined, EditOutlined, PlusOutlined, UploadOutlined } from '@ant-design/icons'
+import { EditOutlined, PlusOutlined, UploadOutlined } from '@ant-design/icons'
 import { useTranslation } from 'react-i18next'
 import { ColumnsType } from 'antd/es/table'
 import { IUser } from '~/types/user.interface.ts'
@@ -50,7 +50,7 @@ const UserList: React.FC = () => {
     ]
   })
   const permissionAddUser = useMemo(() => {
-    return hasPermission([ROLE.MANAGER, ROLE.SYSTEM_ADMIN], userInfo?.groupProfiles)
+    return hasPermission([ROLE.MANAGER, ROLE.SYSTEM_ADMIN, ROLE.SUB_MANAGER], userInfo?.groupProfiles)
   }, [userInfo])
   // const [pagingAndSort, setPagingAndSort] = useState<{ paging: IPaging; sorts: ISort[] }>({
   //   paging: {
